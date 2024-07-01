f36be94e1ddc184ba13903a15bd4eadab3ca70e8
Jose Ortale <jose.ortale@conjure.co.uk>
2024-07-02 00:47:30 +0100

    Add HttpLogginInterceptor

--- app/src/main/java/com/example/beering/data/network/NetworkModule.kt
+++ app/src/main/java/com/example/beering/data/network/NetworkModule.kt
@@ -1,20 +1,32 @@
 package com.example.beering.data.network
 
 import com.example.beering.BuildConfig
+import com.example.beering.BuildConfig.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
     fun provideRetrofit(): Retrofit {
+        val loggingInterceptor = HttpLoggingInterceptor()
+
+        if (BuildConfig.DEBUG) loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        else loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.NONE)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor).build()
         return Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
+            .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
